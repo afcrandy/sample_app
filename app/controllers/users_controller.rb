735ca1fe23c78873225434387355fde82e29878d
Andrew Reid <andrew.o.reid@gmail.com>
2014-03-04 11:13:32 +0000

    Minor refactoring and updated Destroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 	before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
 	before_action :correct_user,   only: [:edit, :update]
 	before_action :admin_user,     only: :destroy
+	before_action :new_user,       only: [:new, :create]
 
 	def index
 		@users = User.paginate(page: params[:page])
@@ -39,9 +40,14 @@ class UsersController < ApplicationController
 	end
 
 	def destroy
-		User.find(params[:id]).destroy
-		flash[:success] = "User deleted."
-		redirect_to users_url
+		destroyee = User.find(params[:id])
+		if current_user?(destroyee)
+			flash[:error] = "Admin suicide warning: Cannot delete yourself"
+		else
+			destroyee.destroy
+			flash[:success] = "User deleted."
+		end
+		redirect_to users_path
 	end
 
 
@@ -67,5 +73,9 @@ class UsersController < ApplicationController
 
 		def admin_user
 			redirect_to(root_url) unless current_user.admin?
+		end
+
+		def new_user
+			redirect_to root_url if signed_in?
 		end
 end
